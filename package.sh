1f29e787d052aacfa3fbd2fcde12520d630daebb
Bharath Upadhya <1992bharath@gmail.com>
2018-06-18 13:35:28 +0530

    adds comments

--- package.sh
+++ package.sh
@@ -13,11 +13,16 @@ init_ve() {
 
 package() {
   local arch=$(uname -m)
+  # 32-bit binaries are built on 32-bit docker containers running on 64-bit
+  # machines. So, aarch32 is explicitly sent to package.sh, so that the binary
+  # is added in the correct folder.
   if [ ! -z "$BINARY_ARCHITECTURE" ]; then
     arch="$BINARY_ARCHITECTURE"
   fi
   sudo rm -r dist/$arch/linux || true
   pyinstaller --distpath dist/$arch/linux --clean --hidden-import=requests main.py
+  # x86_64 is the default architecture. So, the binary is copied to the default
+  # path dist/main/
   if [ "$arch" == "x86_64" ]; then
     sudo rm -r dist/main || true
     mkdir -p dist/main
