bd243c5e26f0a143ed0d360354bc646378408e36
bharath92 <1992.bharath@gmail.com>
2018-06-18 07:45:11 +0000

    accept variable to build binaries for aarch32 arch

--- package.sh
+++ package.sh
@@ -1,20 +1,21 @@
 #!/bin/bash -e
 
+export BINARY_ARCHITECTURE="$1"
 readonly VE_LOCATION=/tmp/cexec_pkg_ve
 
 init_ve() {
   local arch=$(uname -m)
   virtualenv -p /usr/bin/python $VE_LOCATION
   source $VE_LOCATION/bin/activate
-  # TODO: remove this check once pyinstaller 3.4 is released
-  if [ "$arch" != "aarch64" ]; then
-    pip install pyinstaller==3.3
-  fi
+  pip install pyinstaller==3.3.1
   pip install -r requirements.txt
 }
 
 package() {
   local arch=$(uname -m)
+  if [ ! -z "$BINARY_ARCHITECTURE" ]; then
+    arch="$BINARY_ARCHITECTURE"
+  fi
   sudo rm -r dist/$arch/linux || true
   pyinstaller --distpath dist/$arch/linux --clean --hidden-import=requests main.py
   if [ "$arch" == "x86_64" ]; then
